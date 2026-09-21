1952bc60df07a51f46f84fae63bd45098e234f21
Jorge Lopes <75101037+Jec0@users.noreply.github.com>
2026-09-21 15:36:52 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py seed_acervo --sem-capas
+
+python manage.py importar_livros --alvo 500
